db968be5017d787e2aefec44766a6dab9ed04722
Diego Rodrigues Barbosa <diego@pop-os.localdomain>
2022-12-29 22:16:40 -0300

    Initial project

---
+++ cambio-service/src/test/java/com/diego/CambioServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diego;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CambioServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
